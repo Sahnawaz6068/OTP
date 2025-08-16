d5cf9d6006cce14ad823defa91d65d0f075a90eb
Sahnawaz Hussain <sahnawaz643786@gmail.com>
2025-08-16 18:50:00 +0530

    Done validation of OTP with -->Their coresponding phone number

--- src/controllers/otpController.js
+++ src/controllers/otpController.js
@@ -1,30 +1,39 @@
 import checkNumber from "../utils/checkNumber.js";
 import generateOtp from "../utils/generateOtp.js";
 
+const otpStore = {}; 
+
 const generateOTP = async (req, res) => {
   const phoneNo = req.body.phone;
 
   try {
-    const NumberResponse = checkNumber(phoneNo); //Check no. is valid or not
+    const NumberResponse = checkNumber(phoneNo); //Check no. is valId or not
 
     if (!NumberResponse) {
       throw new Error("Plese Enter valid Phone Number");
     }
 
-    const OtpResponse = generateOtp(); //Generate OTP
-    console.log(OtpResponse);
-    
+    const otp = generateOtp(); //Generate
+    console.log(otp);
+
+    otpStore[phoneNo] = { 
+      otp,//Store otp in Local Storage
+      expiresAt: Date.now() + 2 * 60 * 1000, 
+      used:false
+    };
+
+    console.log("Otp store "+otpStore);
+
     const privacyNo = "xxxxxx" + phoneNo.slice(-4);
 
     res.status(200).json({
       message: `OTP sent successfully to ${privacyNo}`,
     });
-  } 
-  catch (err) {
+  } catch (err) {
     res.status(400).json({
       error: err.message,
     });
   }
 };
 
-export { generateOTP };
+export { generateOTP,otpStore };
--- src/controllers/varifyOtpController.js
+++ src/controllers/varifyOtpController.js
@@ -1,22 +1,34 @@
-
+import { otpStore } from "./otpController.js";
 
 const verifyOtpControllers = async (req, res) => {
-//  const otpInput=req.body.otp;
+  const { phone, otp: otpInput } = req.body;
 
- try{
-    // varifyOtp();
-    // if(otpInput!==otp){
-    //     throw new Error("Plese enter valid Email");
-    // }
-    res.status(200).json({
-        message:"Otp varified"
-    })
- }
- catch(err){
-    res.status(400).json({ 
-        erorr:err.message
-    })
- }
+  try {
+    const OtpData = otpStore[phone]; // Get OTP data for that phone --->    otpStore={phone1:{otp,experyAt,used},phone2:{otp,experyAt,used},}
+
+    if (!OtpData) {
+      return res.status(400).json({ error: "No OTP found for this number" });
+    }
+
+    if (Date.now() > OtpData.expiresAt) {
+      return res.status(400).json({ error: "OTP expired" });
+    }
+
+    if (OtpData.used) {
+      return res.status(400).json({ error: "OTP already used" });
+    }
+
+    if (otpInput !== OtpData.otp) {
+      return res.status(400).json({ error: "Invalid OTP" });
+    }
+
+    // Mark OTP as used
+    OtpData.used = true;
+
+    return res.status(200).json({ message: "OTP verified successfully" });
+  } catch (err) {
+    return res.status(500).json({ error: err.message });
+  }
 };
 
 export { verifyOtpControllers };
